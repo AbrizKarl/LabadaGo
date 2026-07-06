c7fd900085801ea77cf0df14500756406dc902d8
Karl Andrei Abriz <beingkarl10@gmail.com>
2026-07-06 23:53:25 +0800

    Initial Spring Boot backend project setup

---
+++ Backend/src/main/java/edu/cit/Abriz/LabadaGo/LabadaGoApplication.java
@@ -0,0 +1,15 @@
+package edu.cit.Abriz.LabadaGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// this is the entry point of our whole backend app
+@SpringBootApplication
+public class LabadaGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabadaGoApplication.class, args);
+    }
+
+}
+
